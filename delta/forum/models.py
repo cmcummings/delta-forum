053b90902df8c281bef47755d73cabc6fc0d0cfa
gestalt8003 <cmcummings2003@gmail.com>
2018-11-05 22:02:09 -0600

    update

--- delta/forum/models.py
+++ delta/forum/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Subforum(models.Model):
+    category = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+
+class Thread(models.Model):
+    subforum = models.ForeignKey(Subforum, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+
+class Reply(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
--- delta/forum/views.py
+++ delta/forum/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Thread, Reply
 
 def home(request):
     return render(request, 'forum/home.html')
 
 def thread(request):
+    thread_id = request.GET.get("id")
+    thread = Thread.objects.filter(id=thread_id)
+    print(thread.title)
+
     # TODO GET for thread
     return render(request, 'forum/thread.html')
